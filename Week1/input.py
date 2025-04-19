49b73af1ad448b6214a4f34e104a687ff5992b53
gibgibbb <diazkristian321@gmail.com>
2025-04-19 21:49:18 +0800

    Add user input handling in input.py, including name and age prompts with typecasting for age.

---
+++ Week1/input.py
@@ -0,0 +1,13 @@
+# Input - Accep user input 
+# uses input() which prompts user to enter data then returns the entered data as string
+
+name = input("What is your name?: ")
+
+print(f"Your name is {name}")
+
+# TEST - convert the string to an int
+
+age = int(input("What is your age?: "))
+
+print(f"Your age is {age}")
+print(type(age))
