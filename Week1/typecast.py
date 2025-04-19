fbe94b403f103398f8d9f13ccd4a099ba61ae8d1
gibgibbb <diazkristian321@gmail.com>
2025-04-19 18:25:54 +0800

    Add typecasting examples in typecast.py demonstrating conversion between int, float, and string data types.

---
+++ Week1/typecast.py
@@ -0,0 +1,19 @@
+# Typecasting = converting one data type to another data type
+# Example: int -> str, float -> str, etc
+
+name = "Kristian"
+age = 20
+gpa = 3.0
+
+# float -> int
+gpa = int(gpa)
+print(gpa)
+
+# int -> float
+age = float(age)
+print(age)
+
+# int -> string
+age = str(age)
+print(age)
+print(type(age))
