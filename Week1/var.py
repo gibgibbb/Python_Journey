dbf3f003f9759665f1b630e8324f0bfd2f2a81a2
gibgibbb <diazkristian321@gmail.com>
2025-04-19 17:19:33 +0800

    Add variable lessons in var.py with examples of string, integer, float, and boolean

---
+++ Week1/var.py
@@ -0,0 +1,19 @@
+# Variable Lessons
+
+# String
+first_name = "kristian"
+
+# F-strings are string literals prefixed with 'f' or 'F' that contain expressions inside curly braces {}
+print(f"Hello {first_name}")
+# It can be in this format but it looks weird "print("Hello " + first_name)""
+
+# Integers
+age = 18
+
+# Float
+price = 99.99
+
+# Boolean
+is_Kristian = True
+
+
