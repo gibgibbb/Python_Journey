2ca203235ed417ae8b4a10dd08abe30bb00c475e
gibgibbb <diazkristian321@gmail.com>
2025-04-20 18:25:31 +0800

    Add rectangle area calculation script in inputExercise1.py, prompting user for width and length inputs.

---
+++ Week1/inputExercise1.py
@@ -0,0 +1,8 @@
+# Calculate Rectangle Area (w x h)
+
+width = int(input("What is the Rectangle's Width?: "))
+length = int(input("What is the Rectangle's Length?: "))
+
+area = width * length
+
+print(f"The Area is {area}")
