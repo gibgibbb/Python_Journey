b6254e0db082d7fbee83c0549899c42671502b24
gibgibbb <diazkristian321@gmail.com>
2025-04-21 15:58:57 +0800

    Add arithmetic operations in arithmetic.py, including square root calculation and commented-out examples for rounding, absolute value, power, and min/max functions.

---
+++ Week1/arithmetic.py
@@ -0,0 +1,19 @@
+# Built in mathlib
+
+import math
+
+x = 3.14
+y = 5
+z = 12
+
+#result = round(x)
+#result = abs(y)
+#result = pow(y, y)
+#result = max(x, y, z)
+#result = min(x, y, z)
+#print(result)
+
+print(math.sqrt(z))
+
+
+
